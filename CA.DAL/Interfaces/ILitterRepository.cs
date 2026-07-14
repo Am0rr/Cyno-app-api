90c11a71803850b3784c885b7d6afe276c15ac25
Am0rr <itsaizel468@gmail.com>
2026-07-14 14:45:34 +0300

    feat(DAL): create ILitterRepository

---
+++ CA.DAL/Interfaces/ILitterRepository.cs
@@ -0,0 +1,11 @@
+using CA.DAL.Entities;
+using CA.DAL.Enums;
+
+namespace CA.DAL.Interfaces;
+
+public interface ILitterRepository
+{
+    Task<Litter?> GetByIdAsync(Guid id);
+    Task<(List<Litter> items, int totalCount)> GetByBreederAsync(
+        Guid breederId, LitterStatus status, int pageSize, int pageNumber);
+}
