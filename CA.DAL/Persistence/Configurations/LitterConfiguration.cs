df22289e6430d1447cbd1cbfef9ffeb1faaf7f76
Am0rr <itsaizel468@gmail.com>
2026-07-14 14:15:51 +0300

    feat(DAL): add LitterConfiguration

---
+++ CA.DAL/Persistence/Configurations/LitterConfiguration.cs
@@ -0,0 +1,19 @@
+using CA.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CA.DAL.Persistence.Configurations;
+
+public class LitterConfiguration : IEntityTypeConfiguration<Litter>
+{
+    public void Configure(EntityTypeBuilder<Litter> builder)
+    {
+        builder.ToTable("Litters");
+
+        builder.HasKey(l => l.Id);
+
+        builder.Property(l => l.CreatedAt).IsRequired();
+        builder.Property(l => l.BreederId).IsRequired();
+        builder.Property(l => l.Status).IsRequired().HasConversion<string>();
+    }
+}
