06acbd3aca3efd66b3706694803088697b0c4d0f
Am0rr <itsaizel468@gmail.com>
2026-07-14 15:08:41 +0300

    feat(DAL): add migrations

---
+++ CA.DAL/Migrations/20260714120807_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CA.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Benefits",
+                columns: table => new
+                {
+                    BreederId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FreeLimit = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsedCount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Benefits", x => x.BreederId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Litters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BreederId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Litters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntityId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Action = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Benefits");
+
+            migrationBuilder.DropTable(
+                name: "Litters");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
