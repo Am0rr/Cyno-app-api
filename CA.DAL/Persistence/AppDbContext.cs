3da8605d0a9c7722fc4daf01911447bf51283e46
Am0rr <itsaizel468@gmail.com>
2026-07-14 14:25:39 +0300

    feat(DAL): update AppDbContext with DbSets

--- CA.DAL/Persistence/AppDbContext.cs
+++ CA.DAL/Persistence/AppDbContext.cs
@@ -1,3 +1,4 @@
+using CA.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CA.DAL.Persistence;
@@ -5,9 +6,13 @@ namespace CA.DAL.Persistence;
 public class AppDbContext(DbContextOptions<AppDbContext> options)
     : DbContext(options)
 {
+    public DbSet<Litter> Litters { get; set; }
+    public DbSet<BreederBenefit> Benefits { get; set; }
+    public DbSet<AuditLog> Logs { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
