5b71cde0df6cc19eb5327f22ece63cf35e795f9c
Am0rr <itsaizel468@gmail.com>
2026-07-14 17:05:00 +0300

    feat(BLL): add INotificationService in DI

--- CA.BLL/DependencyInjection.cs
+++ CA.BLL/DependencyInjection.cs
@@ -9,6 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ILitterService, LitterService>();
+        services.AddScoped<INotificationService, NotificationService>();
 
         return services;
     }
