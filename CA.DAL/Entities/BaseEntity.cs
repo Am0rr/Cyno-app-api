80464019bc4e6b2163a86bbc3982f5351acaf5c8
Am0rr <itsaizel468@gmail.com>
2026-07-14 13:34:54 +0300

    feat(DAL): add BaseEntity

---
+++ CA.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace CA.DAL.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+}
