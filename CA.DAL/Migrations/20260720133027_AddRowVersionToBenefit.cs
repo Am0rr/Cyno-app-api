a91885e379a1f9394b7afd0ea58fbc6e6bd53058
Am0rr <itsaizel468@gmail.com>
2026-07-20 16:31:40 +0300

    chore(DAL): add AddRowVersionToBenefit migration and update database

---
+++ CA.DAL/Migrations/20260720133027_AddRowVersionToBenefit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CA.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToBenefit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Benefits",
+                type: "BLOB",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Benefits");
+        }
+    }
+}
