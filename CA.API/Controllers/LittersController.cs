20d5bd8a50ba519fbae88144d87bb92900e68d50
Am0rr <itsaizel468@gmail.com>
2026-07-21 12:38:04 +0300

    refactor(API): update LittersController with new CurrentBreederContext

--- CA.API/Controllers/LittersController.cs
+++ CA.API/Controllers/LittersController.cs
@@ -1,30 +1,29 @@
 using CA.BLL.DTOs;
 using CA.BLL.Interfaces;
-using CA.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CA.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class LittersController(ILitterService litterService) : ControllerBase
+public class LittersController(ILitterService litterService,
+    ICurrentBreederContext currentBreeder) : ControllerBase
 {
     private readonly ILitterService _litterService = litterService;
+    private readonly ICurrentBreederContext _currentBreeder = currentBreeder;
 
     [HttpPost("{litterId:guid}/publish")]
-    public async Task<ActionResult<PublishResponse>> Publish(
-        Guid litterId, [FromHeader(Name = "X-Breeder-id")] Guid breederId)
+    public async Task<ActionResult<PublishResponse>> Publish(Guid litterId)
     {
-        var result = await _litterService.PublishAsync(litterId, breederId);
+        var result = await _litterService.PublishAsync(litterId, _currentBreeder.BreederId);
 
         return Ok(result);
     }
 
     [HttpGet]
-    public async Task<ActionResult> GetLitters(
-        [FromHeader(Name = "X-Breeder-id")] Guid breederId, [FromQuery] GetLittersRequest request)
+    public async Task<ActionResult<PagedLitterResponse>> GetLitters([FromQuery] GetLittersRequest request)
     {
-        var result = await _litterService.GetLittersAsync(breederId, request);
+        var result = await _litterService.GetLittersAsync(_currentBreeder.BreederId, request);
 
         return Ok(result);
     }
