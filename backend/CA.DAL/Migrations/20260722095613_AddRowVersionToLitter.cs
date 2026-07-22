8582dcbc82632f9e59f8bdb18b632b0b0ba35238
Am0rr <itsaizel468@gmail.com>
2026-07-22 12:56:36 +0300

    feat(DAL): update migrations

---
+++ backend/CA.DAL/Migrations/20260722095613_AddRowVersionToLitter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CA.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToLitter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Litters",
+                type: "BLOB",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Litters");
+        }
+    }
+}
